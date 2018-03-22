a7370e4c145a6f5d419e5d1414c5962617f09f08
Bryan Yu <bryanyu261@gmail.com>
2018-03-22 14:00:18 -0500

    Completed solution to Colors with Navigation challenge

---
+++ ColorsWithNavigation/ColorsWithNavigation/Color.swift
@@ -0,0 +1,15 @@
+//
+//  Color.swift
+//  ColorsWithNavigation
+//
+//  Created by Bryan Yu on 3/22/18.
+//  Copyright © 2018 Bryan Yu. All rights reserved.
+//
+
+import UIKit
+
+struct Color {
+    let name: String
+    let uiColor: UIColor
+    
+}
